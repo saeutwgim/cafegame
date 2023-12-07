aadb682f59794c2a62aa6ed0ae9f959dc5e0f7c5
unknown <shin23557947@gmail.com>
2023-12-07 11:53:52 +0900

    텍스트 가운데 정렬

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 def main():
+    screen_w = 800
+    screen_h = 600
+    
     pygame.init()
     pygame.display.set_caption("pygame")
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((screen_w, screen_h))
     clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 80)
-    tmr = 0
+    font = pygame.font.SysFont("malgungothic", 50)
+    
+    game1 = 0
 
     while True:
-        tmr = tmr + 1
+
         for event in pygame.event.get():
-            if event.type ==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit
-        
-        txt = font.render(str(tmr),True, WHITE)
-        screen.fill(BLACK)
-        screen.blit(txt, [300, 200])
+
+
+
+        txt = font.render("스페이스바를 눌러 시작하시오",True, WHITE)
+        text_rect = txt.get_rect(center=(screen_w/2, screen_h/2))
+        screen.blit(txt, text_rect)
         pygame.display.update()
         clock.tick(10)
 
